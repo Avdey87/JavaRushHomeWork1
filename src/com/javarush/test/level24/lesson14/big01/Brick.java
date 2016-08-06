1609438eaf6d3f66195bdda5c27e244ddda0aeda
Arconoid <avdey87@gmail.com>
2016-08-06 15:28:36 +0300

    Акрканойд

---
+++ src/com/javarush/test/level24/lesson14/big01/Brick.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level24.lesson14.big01;
+
+/**
+ * Created by aavdeev on 05.08.2016.
+ */
+public class Brick extends BaseObject
+{
+    public Brick(double x, double y, double radius)
+    {
+        super(x, y, radius);
+    }
+
+    @Override
+    void draw()
+    {
+
+    }
+
+    @Override
+    void move()
+    {
+
+    }
+}
---
+++ src/com/javarush/test/level24/lesson14/big01/Canvas.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level24.lesson14.big01;
+
+/**
+ * Created by aavdeev on 05.08.2016.
+ */
+public class Canvas
+{
+}
