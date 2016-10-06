e5ac91426f90a5efa35e5d3f2bc53110b6c86f3d
Avdey49 <avdey87@gmail.com>
2016-10-06 17:39:21 +0300

    Задача 27 уровня (задание 4)

--- src/com/javarush/test/level27/lesson15/big01/Tablet.java
+++ src/com/javarush/test/level27/lesson15/big01/Tablet.java
@@ -3,27 +3,43 @@ package com.javarush.test.level27.lesson15.big01;
 import com.javarush.test.level27.lesson15.big01.kitchen.Order;
 
 import java.io.IOException;
+import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * Created by aavdeev on 06.10.2016.
- */
-public class Tablet {
+public class Tablet extends Observable
+{
     private final int number;
-    public static Logger logger = Logger.getLogger(Tablet.class.getName());
-    public Tablet(int number) {
+    final static Logger logger = Logger.getLogger(Tablet.class.getName());
+
+    public Tablet(int number)
+    {
         this.number = number;
     }
-    public void createOrder() {
-        try {
-            ConsoleHelper.writeMessage(new Order(this).toString());
-        } catch (IOException e) {
+
+    public void createOrder()
+    {
+        Order order = null;
+        try
+        {
+            order = new Order(this);
+            ConsoleHelper.writeMessage(order.toString());
+            setChanged();
+            notifyObservers(order);
+        }
+        catch (IOException ee)
+        {
             logger.log(Level.SEVERE, "Console is unavailable.");
         }
     }
-    @Override
-    public String toString() {
-        return "Tablet{" + "number=" + number + '}';
+
+    public int getNumber()
+    {
+        return number;
+    }
+
+    public String toString()
+    {
+        return "Tablet{number=" + number + "}";
     }
 }
