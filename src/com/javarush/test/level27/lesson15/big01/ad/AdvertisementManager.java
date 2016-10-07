0a8129625fabd32a3ab9a913b2e81e76d4507afd
Avdey49 <avdey87@gmail.com>
2016-10-07 14:58:34 +0300

    Задача 27 уровня (задание 9)

--- src/com/javarush/test/level27/lesson15/big01/ad/AdvertisementManager.java
+++ src/com/javarush/test/level27/lesson15/big01/ad/AdvertisementManager.java
@@ -1,9 +1,19 @@
 package com.javarush.test.level27.lesson15.big01.ad;
 
+import com.javarush.test.level27.lesson15.big01.ConsoleHelper;
+
 /**
  * Created by aavdeev on 07.10.2016.
  */
 public class AdvertisementManager
 {
-    private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
+    private int timeSeconds;
+    private final AdvertisementStorage storage = AdvertisementStorage.getInstance();;
+    public AdvertisementManager(int timeSeconds)
+    {
+        this.timeSeconds = timeSeconds;
+    }
+    public void processVideos(){
+        ConsoleHelper.writeMessage("processVideos method calling");
+    }
 }
