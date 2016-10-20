1d0c26d491a64e26f1051c5dc5edb71e82ce26c7
Avdey49 <avdey87@gmail.com>
2016-10-20 10:11:47 +0300

    Задача 28 уровня (задание 7)

--- src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
+++ src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
@@ -16,7 +16,7 @@ public class HHStrategy implements Strategy
     {
         try
         {
-            Document document = Jsoup.connect(URL_FORMAT).get();
+            Document document = Jsoup.connect(URL_FORMAT).header("User-Agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 OPR/40.0.2308.81").referrer("http://google.ru").get();
 
         }
         catch (IOException e)
