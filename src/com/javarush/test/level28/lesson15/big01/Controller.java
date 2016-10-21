acb9000fc2ce5fdf5f22bb3994cbdb039032a2ac
Avdey49 <avdey87@gmail.com>
2016-10-21 11:50:13 +0300

    Задача 28 уровня (задание 12)

--- src/com/javarush/test/level28/lesson15/big01/Controller.java
+++ src/com/javarush/test/level28/lesson15/big01/Controller.java
@@ -1,41 +1,19 @@
 package com.javarush.test.level28.lesson15.big01;
 
-import com.javarush.test.level28.lesson15.big01.model.Provider;
-import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
+import com.javarush.test.level28.lesson15.big01.model.Model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+public class Controller {
 
+    private Model model;
 
-
-public class Controller
-{
-    private Provider[] providers;
-
-    @Override
-    public String toString()
-    {
-        return "Controller{" +
-                "providers=" + Arrays.toString(providers) +
-                '}';
-    }
-
-    public Controller(Provider... providers)
-    {
-        if (providers == null || providers.length == 0)
+    public Controller(Model model) {
+        if (model == null) {
             throw new IllegalArgumentException();
-        this.providers = providers;
+        }
+        this.model = model;
     }
 
-    public void scan() {
-        ArrayList<Vacancy> vacs = new ArrayList<>();
-        try{
-            for (Provider provider:providers){
-                for (Vacancy vacancy:provider.getJavaVacancies("Java")){
-                    vacs.add(vacancy);
-                }
-            }
-        }catch(NullPointerException e){/*NOP*/}
-        System.out.println(vacs.size());
+    public void onCitySelect(String cityName) {
+        model.selectCity(cityName);
     }
 }
