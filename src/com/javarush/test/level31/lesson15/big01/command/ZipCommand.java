0211096ef1866185a4a796c694386ed37c97c15b
Avdey49 <avdey87@gmail.com>
2016-10-24 12:12:06 +0300

    Задача 31 уровня (задание 6)

--- src/com/javarush/test/level31/lesson15/big01/command/ZipCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipCommand.java
@@ -1,7 +1,7 @@
 package com.javarush.test.level31.lesson15.big01.command;
 
 
-abstract class ZipCommand implements Command
+public abstract class ZipCommand implements Command
 {
     @Override
     public void execute() throws Exception
