368e4ce18132ee41cb1aed9f9fb062ada7b91345
Avdey49 <avdey87@gmail.com>
2016-10-25 10:20:29 +0300

    Задача 31 уровня (задание 9)

--- src/com/javarush/test/level31/lesson15/big01/Archiver.java
+++ src/com/javarush/test/level31/lesson15/big01/Archiver.java
@@ -1,26 +1,37 @@
 package com.javarush.test.level31.lesson15.big01;
 
+import com.javarush.test.level31.lesson15.big01.exception.WrongZipFileException;
 
-import com.javarush.test.level31.lesson15.big01.command.ExitCommand;
+import java.io.IOException;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
+public class Archiver {
+    public static void main(String[] args) throws IOException {
 
-public class Archiver
-{
-    public static void main(String[] args) throws Exception
-    {
-        System.out.println("Введите полный путь к файлу архива (куда будем сжимать): ");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String archivePath = reader.readLine();
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(archivePath));
-        System.out.println("Введите путь к файлу, который будем архивировать (что будем сжимать): ");
-        BufferedReader fileNameReader = new BufferedReader(new InputStreamReader(System.in));
-        String archivedFile = fileNameReader.readLine();
-        zipFileManager.createZip(Paths.get(reader.readLine()));
+        Operation operation = null;
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
 
-        ExitCommand exitCommand = new ExitCommand();
-        exitCommand.execute();
+        } while (operation != Operation.EXIT);
+    }
+
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
+
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
