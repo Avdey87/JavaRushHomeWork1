33d4c62c4d5b8044072497bac28b96a00f948f49
Avdey49 <avdey87@gmail.com>
2016-10-27 16:14:28 +0300

    Задача 31 уровня (задание 12)

--- src/com/javarush/test/level31/lesson15/big01/command/ZipRemoveCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipRemoveCommand.java
@@ -3,4 +3,10 @@ package com.javarush.test.level31.lesson15.big01.command;
 
 public class ZipRemoveCommand extends ZipCommand
 {
+    @Override
+    public void execute() throws Exception
+    {
+
+    }
+
 }
