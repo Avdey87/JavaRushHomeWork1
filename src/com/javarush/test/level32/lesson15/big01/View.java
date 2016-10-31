3202d177a8f8be83fa49d1f22e1404e61bc0d3e6
Avdey49 <avdey87@gmail.com>
2016-10-31 15:37:52 +0300

    Задача 32 уровня (задание 7)

--- src/com/javarush/test/level32/lesson15/big01/View.java
+++ src/com/javarush/test/level32/lesson15/big01/View.java
@@ -2,8 +2,10 @@ package com.javarush.test.level32.lesson15.big01;
 
 
 import com.javarush.test.level32.lesson15.big01.listeners.FrameListener;
+import com.javarush.test.level32.lesson15.big01.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -49,7 +51,12 @@ public class View extends JFrame implements ActionListener
 
     public void initEditor()
     {
-
+        htmlTextPane.setContentType("text/html");
+        tabbedPane.addTab("HTML", new JScrollPane(htmlTextPane));
+        tabbedPane.addTab("Текст", new JScrollPane(plainTextPane));
+        tabbedPane.setPreferredSize(new Dimension(800, 600));
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+        getContentPane().add(tabbedPane,BorderLayout.CENTER);
     }
 
     public void initGui()
