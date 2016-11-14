7a973a586fd35a8ae8c59d2633b8be5b351ac6bd
Avdey49 <avdey87@gmail.com>
2016-11-14 11:15:29 +0300

    Задача 34 уровня (задание 10)

--- src/com/javarush/test/level34/lesson15/big01/view/Field.java
+++ src/com/javarush/test/level34/lesson15/big01/view/Field.java
@@ -1,12 +1,15 @@
 package com.javarush.test.level34.lesson15.big01.view;
 
 
+import com.javarush.test.level34.lesson15.big01.controller.EventListener;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Field extends JPanel
 {
     private View view;
+    private EventListener eventListener;
 
     public Field(View view)
     {
@@ -16,5 +19,10 @@ public class Field extends JPanel
     public void paint(Graphics g)
     {
 
+    }
+
+    public void setEventListener(EventListener eventListener)
+    {
+        this.eventListener = eventListener;
     }
 }
--- src/com/javarush/test/level34/lesson15/big01/view/View.java
+++ src/com/javarush/test/level34/lesson15/big01/view/View.java
@@ -1,6 +1,7 @@
 package com.javarush.test.level34.lesson15.big01.view;
 
 import com.javarush.test.level34.lesson15.big01.controller.Controller;
+import com.javarush.test.level34.lesson15.big01.controller.EventListener;
 
 import javax.swing.*;
 
@@ -22,4 +23,10 @@ public class View extends JFrame {
         setTitle("Сокобан");
         setVisible(true);
     }
+
+    public void setEventListener(EventListener eventListener)
+    {
+        this.field.setEventListener(eventListener);
+    }
+
 }
