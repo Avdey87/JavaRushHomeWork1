91938a34eb333de31b3cdce48eb70800578cf38f
Avdey49 <avdey87@gmail.com>
2016-11-14 11:36:00 +0300

    Задача 34 уровня (задание 12)

--- src/com/javarush/test/level34/lesson15/big01/model/Model.java
+++ src/com/javarush/test/level34/lesson15/big01/model/Model.java
@@ -3,13 +3,34 @@ package com.javarush.test.level34.lesson15.big01.model;
 
 import com.javarush.test.level34.lesson15.big01.controller.EventListener;
 
-public class Model {
+import java.nio.file.Paths;
+
+public class Model
+{
     public static final int FIELD_SELL_SIZE = 20;
     private EventListener eventListener;
+    private GameObjects gameObjects;
+    private int currentLevel = 1;
+    private LevelLoader levelLoader = new LevelLoader(Paths.get("..\\res\\levels.txt"));
 
-
-    public void setEventListener(EventListener eventListener)
-    {
+    public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public GameObjects getGameObjects() {
+        return gameObjects;
+    }
+
+    public void restartLevel(int level) {
+        this.gameObjects = levelLoader.getLevel(level);
+    }
+
+    public void restart() {
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel() {
+        currentLevel = currentLevel + 1;
+        restartLevel(currentLevel);
     }
 }
